c339543ab459004061b244dd36e10aa29c5f32a9
Samuel F <humanoidinshell@gmail.com>
2025-10-08 10:09:25 +0200

    fix(plugin): correct noice config

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -31,7 +31,7 @@ return {
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       { "MunifTanjim/nui.nvim" },
-      { "hrsh7th/nvim-cmp" }
+      { "hrsh7th/nvim-cmp" },
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
