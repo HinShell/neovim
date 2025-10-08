0f2fea08368c96d0bc18e7edff9805839817d21c
Samuel F <humanoidinshell@gmail.com>
2025-10-08 19:26:10 +0200

    fix(plugin): add store.lua

---
+++ lua/plugins/tools/store.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "alex-popov-tech/store.nvim",
+    keys = {
+      { "<leader>S", "<cmd>Store<CR>", desc = "Launch Store" },
+    },
+    dependencies = { "OXY2DEV/markview.nvim" },
+    opts = {},
+    cmd = "Store"
+  }
+}
