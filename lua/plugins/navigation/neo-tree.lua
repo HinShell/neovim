248f51a2548dd571a3601a713dcb5794ae46ce50
Samuel F <humanoidinshell@gmail.com>
2025-10-15 21:19:27 +0200

    perf(plugins): enable lazy loading for markview and neo-tree

--- lua/plugins/navigation/neo-tree.lua
+++ lua/plugins/navigation/neo-tree.lua
@@ -1,28 +1,33 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = { "Neotree" }, -- Charger à la première commande
     keys = {
       { "<leader>e", group = "🗂️  Explorateur" },
       { "<leader>ee", "<cmd>Neotree toggle<CR>", desc = "Explorer NeoTree" },
       { "<leader>ef", "<cmd>Neotree focus<CR>", desc = "Focus explorateur" },
-      options = {
-        winblend = 0,
-        signcolumn = "yes",
-        number = false,
-        relativenumber = false,
-      },
-      position = "left",
-      width = 30,
-      -- Prevent neo-tree from being considered a real listed buffer
-      hide_root_node = true,
-      buflisted = false,
     },
-    branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+      {
+        "s1n7ax/nvim-window-picker",
+        version = "2.*",
+        config = function()
+          require("window-picker").setup({
+            filter_rules = {
+              include_current_win = false,
+              autoselect_one = true,
+              bo = {
+                filetype = { "neo-tree", "neo-tree-popup", "notify" },
+                buftype = { "terminal", "quickfix" },
+              },
+            },
+          })
+        end,
+      },
     },
-    lazy = false,                    -- neo-tree will lazily load itself
   }
 }
--- lua/plugins/tools/markview.lua
+++ lua/plugins/tools/markview.lua
@@ -1,11 +1,14 @@
 return {
   {
     "OXY2DEV/markview.nvim",
-    lazy = false,
+    ft = { "markdown", "md" }, -- Charger seulement pour fichiers Markdown
+    cmd = { "Markview" },      -- Ou si des commandes existent
+    event = "BufRead *.md",    -- Alternative avec pattern
 
     -- For blink.cmp's completion
     -- source
     -- dependencies = {
     --     "saghen/blink.cmp"
+    -- }
   }
 };
