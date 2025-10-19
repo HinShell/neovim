5a68d9f4ec791a387d9796ae1f27602d40927ab6
Samuel F <humanoidinshell@gmail.com>
2025-10-19 12:08:54 +0200

    chore(ai): update Avante config with new models and provider improvements

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -8,8 +8,6 @@ return {
         or "make",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
-    ---@module 'avante'
-    ---@type avante.Config
     opts = {
       -- add any opts here
       -- this file can contain specific instructions for your project
@@ -41,14 +39,14 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "nvim-mini/mini.pick",         -- for file_selector provider mini.pick
+      "nvim-mini/mini.pick",           -- for file_selector provider mini.pick
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua",            -- for file_selector provider fzf
-      "stevearc/dressing.nvim",      -- for input provider dressing
-      "folke/snacks.nvim",           -- for input provider snacks
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
+      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+      "stevearc/dressing.nvim",        -- for input provider dressing
+      "folke/snacks.nvim",             -- for input provider snacks
+      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",        -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
