f1a4b8a50e277f8a34823a288fd00f26bd46c906
Samuel F <humanoidinshell@gmail.com>
2025-12-16 17:38:46 +0100

    fix(floaterm): fix scrolling in terminal

--- lua/plugins/tools/vim-floaterm.lua
+++ lua/plugins/tools/vim-floaterm.lua
@@ -20,13 +20,17 @@ return {
       { "<C-é>", "<cmd>FloatermNext<CR>", mode = "t", desc = "Next terminal" },
       { "<C-&>", "<cmd>FloatermPrev<CR>", mode = "t", desc = "Previous terminal" },
       
-      -- Fix pour PageUp/PageDown dans le terminal
-      { "<PageUp>", "<C-\\><C-n><PageUp>i", mode = "t", desc = "Page Up in terminal" },
-      { "<PageDown>", "<C-\\><C-n><PageDown>i", mode = "t", desc = "Page Down in terminal" },
+      -- Fix pour PageUp/PageDown dans le terminal (scroll naturel)
+      { "<PageUp>", "<C-\\><C-n><C-u>i", mode = "t", desc = "Page Up in terminal" },
+      { "<PageDown>", "<C-\\><C-n><C-d>i", mode = "t", desc = "Page Down in terminal" },
       
       -- Alternatives avec Shift+Arrow pour scroll
       { "<S-Up>", "<C-\\><C-n><C-u>i", mode = "t", desc = "Scroll up in terminal" },
       { "<S-Down>", "<C-\\><C-n><C-d>i", mode = "t", desc = "Scroll down in terminal" },
+      
+      -- Alternatives avec Ctrl+Arrow pour scroll (plus naturel)
+      { "<C-Up>", "<C-\\><C-n><C-u>i", mode = "t", desc = "Scroll up in terminal" },
+      { "<C-Down>", "<C-\\><C-n><C-d>i", mode = "t", desc = "Scroll down in terminal" },
       
       -- Terminaux spécialisés
       { "<leader>tg", "<cmd>FloatermNew --title=Lazygit --width=0.9 --height=0.9 lazygit<cr>", desc = "Launch Lazygit" },
@@ -58,19 +62,33 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "floaterm",
         callback = function()
-          -- Permettre le scroll avec la molette
-          vim.keymap.set('t', '<ScrollWheelUp>', '<C-\\><C-n><ScrollWheelUp>', { buffer = true, silent = true })
-          vim.keymap.set('t', '<ScrollWheelDown>', '<C-\\><C-n><ScrollWheelDown>', { buffer = true, silent = true })
+          -- Configuration pour défilement naturel
+          local opts = { buffer = true, silent = true }
+          
+          -- Scroll avec la molette de souris
+          vim.keymap.set('t', '<ScrollWheelUp>', '<C-\\><C-n><C-y>i', opts)
+          vim.keymap.set('t', '<ScrollWheelDown>', '<C-\\><C-n><C-e>i', opts)
           
           -- Permettre Ctrl+C pour interrompre
-          vim.keymap.set('t', '<C-c>', '<C-c>', { buffer = true, silent = true })
+          vim.keymap.set('t', '<C-c>', '<C-c>', opts)
           
-          -- Escape pour sortir du terminal mode
-          vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { buffer = true, silent = true })
+          -- Escape pour sortir du terminal mode vers mode normal
+          vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
           
-          -- Navigation et commandes utiles en mode terminal
+          -- Navigation window
           vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { buffer = true, silent = true, desc = "Window commands in terminal" })
-          vim.keymap.set('t', '<C-o>', '<C-\\><C-n><C-o>', { buffer = true, silent = true, desc = "One command in terminal" })
+          
+          -- Scroll ligne par ligne (alternatives fiables)
+          vim.keymap.set('t', '<C-y>', '<C-\\><C-n><C-y>i', { buffer = true, silent = true, desc = "Scroll up one line" })
+          vim.keymap.set('t', '<C-e>', '<C-\\><C-n><C-e>i', { buffer = true, silent = true, desc = "Scroll down one line" })
+          
+          -- Scroll avec Ctrl+Shift (plus fiable sur macOS)
+          vim.keymap.set('t', '<C-S-k>', '<C-\\><C-n><C-y>i', { buffer = true, silent = true, desc = "Scroll up one line" })
+          vim.keymap.set('t', '<C-S-j>', '<C-\\><C-n><C-e>i', { buffer = true, silent = true, desc = "Scroll down one line" })
+          
+          -- Scroll avec fonction keys (toujours disponibles)
+          vim.keymap.set('t', '<F9>', '<C-\\><C-n><C-y>i', { buffer = true, silent = true, desc = "Scroll up one line" })
+          vim.keymap.set('t', '<F10>', '<C-\\><C-n><C-e>i', { buffer = true, silent = true, desc = "Scroll down one line" })
         end,
       })
     end,
